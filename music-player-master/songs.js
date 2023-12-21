47b440d56377776ffc6670370cb09a7482e9d957
FrrdevC <154547783+FrrdevC@users.noreply.github.com>
2023-12-21 15:18:33 -0300

    Add files via upload

---
+++ music-player-master/songs.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    src: "./src/Osa - Diamonds n Pearls.mp3",
+    name: "Diamonds n Pearls - Osa",
+  },
+  {
+    src: "./src/Pop Smoke - Imperfections (Interlude) (Audio).mp3",
+    name: "Imperfection Interlude - Pop Smoke",
+  },
+  {
+    src: "./src/Kina - get you the moon (Lyrics) ft. Snow.mp3",
+    name: "Get you the moon - Kina ft.Snow",
+  },
+  {
+    src: "./src/James Arthur - Say You Wont Let Go.mp3",
+    name: "Say you won't let Go - James Arthur",
+  },
+  {
+    src: "./src/Justin Bieber - All That Matters (Official Audio).mp3",
+    name: "All That Matters - Justin Bieber",
+  },
+  {
+    src: "./src/Trevor Daniel - Falling  Tradução  Legendado.mp3",
+    name: "Falling - Trevor Daniel",
+  },
+  {
+    src: "./src/James Arthur - Can I Be Him (Lyric Video).mp3",
+    name: "Can I Be Him? - James Arthur",
+  },
+  {
+    src: "./src/Falling For U - Single.mp3",
+    name: "Falling For U - Peachy",
+  },
+  {
+    src: "./src/Jacquees - Whos.mp3",
+    name: "Whos - Jacquees",
+  },
+];
